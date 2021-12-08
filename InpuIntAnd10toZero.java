c7bc06255fd038fcdabf1d53e661e633338f59c4
samitha1998 <63746091+samitha1998@users.noreply.github.com>
2021-12-09 04:50:04 +0530

    Create InpuIntAnd10toZero.java

---
+++ InpuIntAnd10toZero.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class InpuIntAnd10toZero{
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		System.out.print("Input an integer : ");
+		int num = input.nextInt();	
+			System.out.print("[");
+				for (int i = 0; i <num; i++){
+			System.out.print(i+", ");
+					
+				}
+			System.out.println("\b\b]");		
+	}
+}
