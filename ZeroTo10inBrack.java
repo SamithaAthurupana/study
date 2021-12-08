53935694a95b427055fd18e3d7251445a23601ff
samitha1998 <63746091+samitha1998@users.noreply.github.com>
2021-12-09 01:25:23 +0530

    Create ZeroTo10inBrack.java

---
+++ ZeroTo10inBrack.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ZeroTo10inBrack{
+	public static void main(String[] args) {
+			System.out.print("[");
+				for (int i = 0; i <10; i++){
+					System.out.print(i+", ");							
+				}
+			System.out.println("\b\b]");		
+	}
+}
