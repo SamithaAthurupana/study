6f602928bbd2744c9976558026fe2dc9c2b8bc96
samitha1998 <63746091+samitha1998@users.noreply.github.com>
2021-12-09 01:16:16 +0530

    Create ZeroTo10For.java

---
+++ ZeroTo10For.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ZeroTo10For{
+	public static void main(String[] args) {
+			System.out.println("Start Main");
+				for (int i = 0; i < 10; i++){
+					System.out.println("No : "+i);							
+				}
+			System.out.println("End Main");			
+	}
+}
