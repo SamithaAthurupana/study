6816862652bb5f9500ef609ced83e8648b335e2b
samitha1998 <63746091+samitha1998@users.noreply.github.com>
2021-12-09 06:14:58 +0530

    Update and rename InputNumAndGetFactorial.java to FOR_InputNumAndGetFactorial.java

--- FOR_InputNumAndGetFactorial.java
+++ FOR_InputNumAndGetFactorial.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class InputNumAndGetFactorial{
+class FOR_InputNumAndGetFactorial{
 	public static void main(String[] args) {
 		Scanner input = new Scanner (System.in);
 		System.out.print("Input Number : ");
