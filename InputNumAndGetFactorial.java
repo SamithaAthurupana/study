92bf166fc629c01705fce781cca665a917dc0f12
samitha1998 <63746091+samitha1998@users.noreply.github.com>
2021-12-09 06:11:56 +0530

    Update and rename InputNumAndGetFactOfList.java to InputNumAndGetFactorial.java

--- InputNumAndGetFactorial.java
+++ InputNumAndGetFactorial.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class InputNumAndGetFactOfList{
+class InputNumAndGetFactorial{
 	public static void main(String[] args) {
 		Scanner input = new Scanner (System.in);
 		System.out.print("Input Number : ");
