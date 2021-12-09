e30e6bc0014dd5a4e8148f9ec36c80f826ae6f03
samitha1998 <63746091+samitha1998@users.noreply.github.com>
2021-12-09 05:55:06 +0530

    Create InputNumAndGetFactOfList.java

---
+++ InputNumAndGetFactOfList.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class InputNumAndGetFactOfList{
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		System.out.print("Input Number : ");
+		int num = input.nextInt();
+			System.out.print("[ ");
+		int Fact=1;	
+				for (int i = 1; i <=num; i++){
+					System.out.print(i+" * ");
+					Fact*=i;
+
+				}
+			System.out.println("\b\b] = "+Fact);	
+	}
+}
