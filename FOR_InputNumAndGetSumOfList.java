963cb2662c61d400f21984165c69f42d04ea0ef5
samitha1998 <63746091+samitha1998@users.noreply.github.com>
2021-12-09 06:13:34 +0530

    Update and rename InputNumAndGetSumOfList.java to FOR_InputNumAndGetSumOfList.java

--- FOR_InputNumAndGetSumOfList.java
+++ FOR_InputNumAndGetSumOfList.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class InputNumAndGetSumOfList{
+class FOR_InputNumAndGetSumOfList{
 	public static void main(String[] args) {
 		Scanner input = new Scanner (System.in);
 		System.out.print("Input Number : ");
